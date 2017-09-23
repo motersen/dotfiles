3e5bac85d16873b7c95b7b641288ab8b05b1dc49
Moritz Petersen <moritz.petersen@freenet.de>
2017-09-23 23:50:38 +0200

    Configure rcirc

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -19,6 +19,8 @@
 (require 'init-editor)
 (require 'init-gui)
 
+(require 'init-rcirc)
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
---
+++ emacs/.emacs.d/lisp/init-rcirc.el
@@ -0,0 +1,5 @@
+(setq rcirc-server-alist
+			'(("irc.freenode.net" :port 6697 :encryption tls
+				 :channels ("#freenode"))))
+
+(provide 'init-rcirc)
