d5e5d3f24a5c430f03426a6cda652e44ceb6a6c3
Moritz Petersen <moritz.petersen@freenet.de>
2017-09-24 18:31:33 +0200

    rcirc: implement automatic authentication

--- emacs/.emacs.d/lisp/init-rcirc.el
+++ emacs/.emacs.d/lisp/init-rcirc.el
@@ -2,4 +2,39 @@
 			'(("irc.freenode.net" :port 6697 :encryption tls
 				 :channels ("#freenode"))))
 
+(setq rcirc-authinfo
+			'(("freenode" nickserv "motersen" rcirc-get-password)))
+
+(add-hook 'rcirc-mode-hook
+					(lambda ()
+						(set (make-local-variable 'scroll-conservatively)
+								 8192)))
+
+(defun rcirc-get-password (host nick)
+	(replace-regexp-in-string "\n\\'" ""
+														(shell-command-to-string (format "pass %s/%s" nick host))))
+
+(defun rcirc-replace-passwords (rcirc-authenticate &rest args)
+	(let (full-authinfo)
+		(dolist (record rcirc-authinfo)
+			(setq full-authinfo (cons
+													 (let ((method (nth 1 record))
+																 (password (nth 3 record)))
+														 (if (and (not (eq method 'chanserv))
+																			(functionp password))
+																 (let ((host (nth 0 record))
+																			 (nick (nth 2 record)))
+																	 (append
+																		(list host
+																					method
+																					nick
+																					(funcall password host nick))
+																		(nthcdr 4 record)))
+															 record))
+													 full-authinfo)))
+		(let ((rcirc-authinfo full-authinfo))
+			(funcall rcirc-authenticate))))
+
+(advice-add 'rcirc-authenticate :around #'rcirc-replace-passwords)
+
 (provide 'init-rcirc)
