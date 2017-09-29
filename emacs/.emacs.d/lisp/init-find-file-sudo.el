9a3718f16444b0b7178041b852703340f1aff494
Moritz Petersen <moritz.petersen@freenet.de>
2017-09-29 21:17:33 +0200

    emacs: fix find-file-sudo
    Ensure the file exists before trying to find it as root. This fixes the problem
    of an endless loop that used to occur e.g. when composing a new message in mu4e.
    
    Convert to the new advice-add mechanism.

--- emacs/.emacs.d/lisp/init-find-file-sudo.el
+++ emacs/.emacs.d/lisp/init-find-file-sudo.el
@@ -1,7 +1,10 @@
-(defadvice find-file (after find-file-sudo activate)
+(defun find-file-sudo (buffer-file-name &optional wildcards)
 	"Find file as root if necessary."
-	(unless (and buffer-file-name
-							 (file-writable-p buffer-file-name))
+	(when (and buffer-file-name
+						 (file-exists-p buffer-file-name)
+						 (not (file-writable-p buffer-file-name)))
 		(find-alternate-file (concat "/sudo:root@localhost:" buffer-file-name))))
+
+(advice-add 'find-file :after #' find-file-sudo)
 
 (provide 'init-find-file-sudo)
