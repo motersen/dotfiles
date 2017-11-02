b99af9ebe4d33ca49ff40044192ce673438b2783
Moritz Petersen <moritz.petersen@freenet.de>
2017-11-02 17:44:04 +0100

    conkeror: add subreddit url handler
    Implement a url handler that transforms input looking like a subreddit into a
    url to reddit.

--- conkeror/.conkerorrc/webjumps.js
+++ conkeror/.conkerorrc/webjumps.js
@@ -1,1 +1,9 @@
 define_webjump("dictcc", "http://www.dict.cc/?s=%s");
+
+function read_url_subreddit_handler (input) {
+	if (/^r\/\w+$/.test(input))
+		return "https://reddit.com/"+input;
+	return null;
+}
+
+read_url_handler_list.push(read_url_subreddit_handler);
