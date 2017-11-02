5054c042b1be50a3b2cca5a6e919e67527180753
Moritz Petersen <moritz.petersen@freenet.de>
2017-11-02 18:09:50 +0100

    conkeror: Use ternary operator for url handlers

--- conkeror/.conkerorrc/webjumps.js
+++ conkeror/.conkerorrc/webjumps.js
@@ -1,15 +1,15 @@
 define_webjump("dictcc", "http://www.dict.cc/?s=%s");
 
 function read_url_subreddit_handler (input) {
-	if (/^r\/\w+$/.test(input))
-		return "https://reddit.com/"+input;
-	return null;
+	return /^r\/\w+$/.test(input)
+		? "https://reddit.com/"+input
+		: null;
 }
 
 function read_url_ddg_bang_handler (input) {
-	if (/^!/.test(input))
-		return read_url_make_default_webjump_handler("duckduckgo")(input);
-	return null;
+	return /^!/.test(input)
+		? read_url_make_default_webjump_handler("duckduckgo")(input)
+		: null;
 }
 
 read_url_handler_list.push(read_url_make_blank_url_handler(homepage));
