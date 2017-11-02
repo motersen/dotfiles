074455f2f98255b23ba44647ac4b1f3ca756e89a
Moritz Petersen <moritz.petersen@freenet.de>
2017-11-02 18:03:06 +0100

    conkeror: Move lost comment

--- conkeror/.conkerorrc/styles.js
+++ conkeror/.conkerorrc/styles.js
@@ -1,10 +1,9 @@
-// Github.com
-
 var sheets = get_home_directory();
 
 sheets.append(".conkerorrc");
 sheets.append("stylesheets");
 
+// Github.com
 var gh = Cc["@mozilla.org/file/local;1"].createInstance(Ci.nsILocalFile);
 gh.initWithFile(sheets);
 gh.append("github.css");
